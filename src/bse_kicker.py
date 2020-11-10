e765237a5215d72aa8619e03a8f5ef3c2310307f
Alessandro Alberto Trani <aatrani@gmail.com>
2020-11-10 20:04:01 +0900

    rationalizing stuff

---
+++ src/bse_kicker.py
@@ -0,0 +1,101 @@
+import numpy as np
+from amuse.units import constants, units
+from amuse.datamodel import Particles
+
+
+
+class BSEPairKicker():
+
+    def __init__(self, kicks_per_orbit=100.0, tides=False, mass_loss=False,
+                 mass_accretion=False, common_envelope=False):
+        """
+        Initialized the kicker
+        :param kicks_per_orbit: needed to estimate the next optimal timestep #TODO
+        :param tides: enable/disable tides
+        :param mass_loss: enable/disable
+        :param mass_accretion: enable/disable
+        :param common_envelope: enable/disable
+        """
+        self.mass_loss = mass_loss
+        self.tides = tides
+        self.common_envelope = common_envelope
+        self.mass_accretion = mass_accretion
+        self.kicks_per_orbit = kicks_per_orbit
+
+    def kick_pair(self, stellar_pair, dt):
+        """
+        Kicks the pair of particle according to the enabled physical processes
+        :param stellar_pair: the stellar pair or binary that is evolving via stellar evolution
+        :param dt: the timestep of the kick step
+        :return:
+        """
+        self.pair = stellar_pair
+        self.pos_com = stellar_pair.center_of_mass()
+        self.vel_com = stellar_pair.center_of_mass_velocity()
+        self.dt = dt
+        self.calculate_useful_stuff()
+
+        if self.mass_accretion: self.kick_from_accretion()
+        if self.tides: self.kick_from_eccentric_tides()
+
+    def calculate_useful_stuff(self):
+        self.pos = self.pair[1].position - self.pair[0].position
+        self.vel = self.pair[1].velocity - self.pair[0].velocity
+
+        self.r = self.pos.length()
+        self.v = self.vel.length()
+        self.inv_r = 1.0 / self.r
+
+        self.rvec = self.pos * self.inv_r
+        self.rdot = (self.rvec * self.vel).sum() * self.rvec
+        self.rdot_mag = self.rdot.length()
+
+        self.vth = self.vel - self.rdot
+        self.vth_mag = self.vth.length()
+        self.vth_vec = self.vth / self.vth_mag
+
+    def kick_from_accretion(self):
+        """
+        Kick from accretion in the tangential direction.
+        The star recoils due to momentum conservation
+        Assuming dVth1/Vrel = -dm1/m1
+
+        Particles need to have this attribute:
+        dmdt_acc: instantaneus mass accretion rate [mass/time]
+        """
+
+        accth_mag = -self.v * self.pair.dmdt_acc / self.pair.mass
+        kick0 = -self.vth_vec * accth_mag[0] * self.dt
+        kick1 = self.vth_vec * accth_mag[1] * self.dt
+
+        self.pair[0].velocity += kick0
+        self.pair[1].velocity += kick1
+
+    def kick_from_eccentric_tides(self):
+        """
+        Kick from tides due to eccentric orbit (Equilibrium tide model, Hut 1981)
+
+        Particles to have following attributes:
+        radius: stellar radius [length]
+        kaps: apsidal constant [no unit]
+        taulag: time-lag [time]
+        """
+
+        inv_r_7 = self.inv_r**7
+        r0_5 = self.pair[0].radius ** 5
+        r1_5 = self.pair[1].radius ** 5
+        m0_2 = self.pair[0].mass * self.pair[0].mass
+        m1_2 = self.pair[1].mass * self.pair[1].mass
+
+        ftr = -3.0 * inv_r_7 * constants.G * ((m1_2 * r0_5 * self.pair[0].kaps + m0_2 * r1_5 * self.pair[1].kaps) +  # Non-dissipative
+                                              3 * self.inv_r * self.rdot_mag *
+                                              (m1_2 * r0_5 * self.pair[0].kaps * self.pair[0].taulag
+                                               + m0_2 * r1_5 * self.pair[1].kaps * self.pair[1].taulag))  # Dissipative
+
+        hutforce = ftr * self.inv_r * self.pos
+        kick0 = (1.0 / self.pair[0].mass) * hutforce * self.dt
+        kick1 = (-1.0 / self.pair[1].mass) * hutforce * self.dt
+
+        self.pair[0].velocity += kick0
+        self.pair[1].velocity += kick1
+
---
+++ src/dynbin_comenv.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+from amuse.units import units, constants, nbody_system
+from amuse.ext.orbital_elements import new_binary_from_orbital_elements
+from amuse.ext.orbital_elements import orbital_elements_from_binary
+from amuse.ext.orbital_elements import orbital_elements_from_binary
+from amuse.community.hermite.interface import Hermite
+import numpy
+
+from dynbin_common import (
+    make_binary_star, new_option_parser,
+    mass_loss_rate, dadt_masschange, dedt_masschange, get_period
+)
+
+
+def dadt_comenv(a, e, K):
+    e2 = e * e
+    da_dt = K / a * 2 * (1 + e2) / (1 - e2) ** 1.5
+    return da_dt
+
+
+def dedt_comenv(a, e, K):
+    e2 = e * e
+    de_dt = K / (a * a) * 2 * e / (1 - e2) ** 0.5
+    return de_dt
+
+def kick_stars_comenv(stars, dt, K):
+    stars.move_to_center()
+    pos = stars[1].position - stars[0].position
+    vel = stars[1].velocity - stars[0].velocity
+
+    r = pos.length()
+    v = vel.length()
+    vvec = vel / v
+
+    acc = K/(r*r) * v * vvec
+    mtot = stars[0].mass + stars[1].mass
+
+    stars[0].velocity += -acc*dt * stars[1].mass/mtot
+    stars[1].velocity += acc*dt * stars[0].mass/mtot
+
+def kick_stars_comenv2(stars, dt, K, A):
+    stars.move_to_center()
+    pos = stars[1].position - stars[0].position
+    vel = stars[1].velocity - stars[0].velocity
+
+    r = pos.length()
+    v = vel.length()
+    vvec = vel / v
+
+    acc = K/A * v * vvec
+    mtot = stars[0].mass + stars[1].mass
+
+    stars[0].velocity += -acc*dt * stars[1].mass/mtot
+    stars[1].velocity += acc*dt * stars[0].mass/mtot
+
+
+def K_from_eps(eps0, eps_ce, Tce, mu):
+    epsf = eps0 + eps_ce
+    K = mu**2 * (1/epsf**2 -  1/eps0**2) / (16*Tce)
+    return K
+
+
+def check_collisions(stars):
+    pos = stars[1].position - stars[0].position
+    r = pos.length()
+    sumrad = stars.radius.sum()
+    if sumrad > r:
+        print("Collided!")
+        return True
+    else: return False
+
+def evolve_model(end_time, double_star, stars):
+    time = 0 | units.yr
+    dt = 0.05*end_time/1000.
+
+    converter = nbody_system.nbody_to_si(double_star.mass,
+                                         double_star.semimajor_axis)
+
+    gravity = Hermite(converter)
+    gravity.particles.add_particle(stars)
+    to_stars = gravity.particles.new_channel_to(stars)
+    from_stars = stars.new_channel_to(gravity.particles)
+
+    period = get_period(double_star)
+    print("Period =", period.as_string_in(units.yr))
+    print("Mass loss timestep =", dt)
+    print("Steps per period: = {:1.2f}".format(period/dt))
+
+    a_an = [] | units.au
+    e_an = []
+    atemp = double_star.semimajor_axis
+    etemp = double_star.eccentricity
+
+    ###### COMMON ENVELOPE STUFF ###############
+    final_a = 40 | units.RSun
+
+    mu = double_star.mass * constants.G
+    Eps0 = mu / (2 * double_star.semimajor_axis)
+    Eps1 = mu / (2 * final_a)
+
+    # Eps_ce should come from alpha lambda model, but we just fix the final semimajor axis here for simplicity
+    Eps_ce = Eps1 - Eps0
+    print("Eps_ce/Eps0", Eps_ce / Eps0)
+
+    Tce = 1000 | units.yr
+    Kce = K_from_eps(Eps0, Eps_ce, Tce, mu)
+    print("Kce", Kce)
+    Avisc = -Kce * Tce
+    print("Avisc", Avisc.as_string_in(units.RSun ** 2))
+    Rvisc = Avisc.sqrt() / (4 * constants.pi)
+    print("Rvisc", Rvisc.as_string_in(units.RSun))
+    ###### END COMMON ENVELOPE STUFF ###############
+
+    collision = False
+    a = [] | units.au
+    e = [] 
+    m = [] | units.MSun
+    t = [] | units.yr
+    while time < end_time:
+        time += dt
+        if not collision:
+            gravity.evolve_model(time)
+            to_stars.copy()
+            kick_stars_comenv2(stars, dt, Kce, Avisc)
+            from_stars.copy()
+
+            from_stars.copy()
+
+            orbital_elements = orbital_elements_from_binary(stars,
+                                                            G=constants.G)
+
+            collision = check_collisions(stars)
+
+        if atemp.number > 0:
+            dadt = dadt_comenv(atemp, etemp, Kce)
+            dedt = dedt_comenv(atemp, etemp, Kce)
+
+            atemp = atemp + dadt*dt
+            etemp = etemp + dedt*dt
+
+        if collision and atemp.number < 0: break
+
+        a_an.append(atemp)
+        e_an.append(etemp)
+        a.append(orbital_elements[2])
+        e.append(orbital_elements[3])
+        m.append(stars.mass.sum())
+        t.append(time)
+        print("time=", time.in_(units.yr),
+              "a=", a[-1].in_(units.RSun),
+              "e=", e[-1],
+              "m=", stars.mass.in_(units.MSun), end="\r")
+
+    gravity.stop()
+    from matplotlib import pyplot
+    import seaborn as sns
+    sns.set(font_scale=1.33)
+    sns.set_style("ticks")
+
+    fig, axis = pyplot.subplots(nrows=2, sharex=True)
+    axis[0].plot(t.value_in(units.yr), a.value_in(units.RSun), label="nbody k=0")
+    axis[0].plot(t.value_in(units.yr), a_an.value_in(units.RSun), label="analytic")
+    axis[0].set_ylabel("semimajor axis [$R_\odot$]")
+    axis[0].legend()
+
+    axis[1].plot(t.value_in(units.yr), e)
+    axis[1].plot(t.value_in(units.yr), e_an)
+    axis[1].set_ylabel("eccentricity")
+
+    axis[1].set_xlabel("time [yr]")
+    axis[0].set_xlabel("time [yr]")
+
+    pyplot.tight_layout()
+    pyplot.subplots_adjust(hspace=0.0)
+    pyplot.savefig("comenv2.png")
+    pyplot.show()
+
+
+def main():
+    m1, m2 = 80 | units.MSun, 55 | units.MSun
+    a0 = 4000 | units.RSun
+    e0 = 0.9
+
+    o, arguments = new_option_parser(M_default=m1, m_default=m2,
+                                     a_default=a0, e_default=e0).parse_args()
+    double_star, stars = make_binary_star(
+        o.mprim, o.msec, o.semimajor_axis, o.eccentricity,
+    )
+    end_time = 1000.0 | units.yr
+    evolve_model(end_time, double_star, stars)
+
+
+if __name__ == "__main__":
+    main()
